404e8c945a84ea9418f856cc79ed601fd0e53426
encadyma <encadyma@gmail.com>
2016-01-20 17:25:12 -0800

    Updated resources.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var Botkit = require('botkit')
+var cleverbot = require('cleverbot.io');
+var cb = new cleverbot('54NkGKDf0f8CwHJv','2X68ZWZi03jw8SkUQRrGKWKDwe0aDj3e');    // This is probably pretty stupid, but k.
 
 // Expect a SLACK_TOKEN environment variable
 var slackToken = process.env.SLACK_TOKEN
@@ -18,16 +20,18 @@ bot.startRTM(function (err, bot, payload) {
   }
 })
 
+cb.create(function (err, session) {})
+
 controller.on('bot_channel_join', function (bot, message) {
   bot.reply(message, "I'm here!")
 })
 
 controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
-  bot.reply(message, 'Hello.')
+  bot.reply(message, 'Hello!')
 })
 
 controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
-  bot.reply(message, 'Hello.')
+  bot.reply(message, 'Hello!')
   bot.reply(message, 'It\'s nice to talk to you directly.')
 })
 
@@ -44,24 +48,11 @@ controller.hears('help', ['direct_message', 'direct_mention'], function (bot, me
   bot.reply(message, help)
 })
 
-controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
-  var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
-  var attachments = [{
-    fallback: text,
-    pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
-    title: 'Host, deploy and share your bot in seconds.',
-    image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
-    title_link: 'https://beepboophq.com/',
-    text: text,
-    color: '#7CD197'
-  }]
-
-  bot.reply(message, {
-    attachments: attachments
-  }, function (err, resp) {
-    console.log(err, resp)
-  })
-})
+controller.hears(['/(hey there)/i'], ['direct_message', 'direct_mention'], function (bot, message) {
+  bot.reply(message, cb.ask(message, function (err, response) {
+    return response;
+  }));
+}))
 
 controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
   bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
